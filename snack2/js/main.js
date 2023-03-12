5ef6f75333051ec4eb9f9c5479749b4b7d27a98e
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-12 12:55:57 +0100

    fine esercizio

--- snack2/js/main.js
+++ snack2/js/main.js
@@ -31,21 +31,25 @@ const teams = [
     },
 ];
 
-
+const display = []
 let NewTeam;
+let finalResult;
 for (let i = 0; i < teams.length; i++) {
     let risks = teams.map(risk => risk.falli)
-    risks =  Math.floor(Math.random() * 100)
+    risks = Math.floor(Math.random() * 100)
     let points = teams.map(point => point.punti)
     points = Math.floor(Math.random() * 100)
+
     NewTeam = {
-    ...teams[i],
-    punti: points,
-    falli: risks
+        ...teams[i],
+        punti: points,
+        falli: risks
     }
-    console.log(NewTeam);
-}
+    finalResult = {
+        nome: teams[i].nome,
+        falli: NewTeam.falli
+    }
+    display.push(finalResult);
+};
 
-
-
-
+console.log(display)
